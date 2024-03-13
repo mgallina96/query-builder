5b0e332b86ef208189237de93daceffc992c9af7
Manuel Galina <manuel.gallina@3ds.team>
2024-03-13 18:03:18 +0100

    fix: update package info files

--- src/query_builder/shared/models/__init__.py
+++ src/query_builder/shared/models/__init__.py
@@ -4,15 +4,21 @@ from typing import Any, Callable
 class FieldMap:
     name: str
     database_column: Any
+    blocked_operators: set[str]
 
     _transform_function: Callable
 
     def __init__(
-        self, name: str, database_column: Any, transform_function: Callable = None
+        self,
+        name: str,
+        database_column: Any,
+        transform_function: Callable = None,
+        blocked_operators: set[str] = None,
     ):
         self.name = name
         self.database_column = database_column
         self._transform_function = transform_function or (lambda x: x)
+        self.blocked_operators = blocked_operators or set()
 
     def transform(self, value: Any) -> Any:
         if isinstance(value, list):
